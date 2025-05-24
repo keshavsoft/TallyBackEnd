3b122f0bf1b4845ade38722081c7c04ad0ca4c12
keshavsoft <nknnkumar@yahoo.com>
2025-05-24 15:46:09 +0530

    empty

--- V1/Sunil/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-import { router as routerFromItemWithGroup } from "./ItemWithGroup/routes.js";
-
-router.use("/ItemWithGroup", routerFromItemWithGroup);
-
-export { router };
--- V1/Varshitha/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-import { router as routerFromItemsWithGroups } from "./ItemsWithGroups/routes.js";
-
-router.use("/ItemsWithGroups", routerFromItemsWithGroups);
-
-export { router };
