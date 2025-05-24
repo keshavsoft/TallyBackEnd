de127f292fc0b5ac54e63b8169d6c26dcef2eff3
keshavsoft <nknnkumar@yahoo.com>
2025-05-24 15:49:29 +0530

    ItemsWithGroups perfect

---
+++ V1/ItemsWithGroups/routes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const router = express.Router();
+
+import { router as routerFromRead } from "./Read/routes.js";
+import { router as routerFromInsert } from "./Insert/routes.js";
+import { router as routerFromDelete } from "./Delete/routes.js";
+import { router as routerFromAlter } from "./Alter/routes.js";
+import { router as routerFromReadSchema } from "./ReadSchema/routes.js";
+import { router as routerFromGroupBy } from "./GroupBy/routes.js";
+
+router.use("/Read", routerFromRead);
+router.use("/Insert", routerFromInsert);
+router.use("/Delete", routerFromDelete);
+router.use("/Alter", routerFromAlter);
+router.use("/ReadSchema", routerFromReadSchema);
+router.use("/GroupBy", routerFromGroupBy);
+
+export { router };
